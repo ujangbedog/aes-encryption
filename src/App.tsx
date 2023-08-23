7e2c812ec4dc5a6d78c7cca50ff7525f611fd86a
ujangbedog <domba88galing@gmail.com>
2023-08-24 01:36:45 +0700

    adding output mode Hexadecimal and edit UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as CryptoJS from 'crypto-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +8,7 @@ function App() {
   const [secretKey, setSecretKey] = useState<string>('');
   const [iv, setIV] = useState<string>('');
   const [mode, setMode] = useState<'ECB' | 'CBC'>('ECB');
+  const [outputMode, setOutputMode] = useState<'Base64' | 'Hex'>('Base64');
   const [encryptedData, setEncryptedData] = useState<string | null>(null);
   const [decryptedData, setDecryptedData] = useState<string | null>(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
@@ -35,19 +36,29 @@ function App() {
       padding: CryptoJS.pad.Pkcs7,
       iv: CryptoJS.enc.Utf8.parse(iv)
     });
-    setEncryptedData(encrypted.toString());
+    const encryptedString = outputMode === 'Base64' ? encrypted.toString() : encrypted.ciphertext.toString(CryptoJS.enc.Hex);
+    setEncryptedData(encryptedString);
   };
 
   const handleDecrypt = () => {
     if (!validateInputs()) return;
 
     const decryptionMode = mode === 'ECB' ? CryptoJS.mode.ECB : CryptoJS.mode.CBC;
-    const decrypted = CryptoJS.AES.decrypt(decryptInput, CryptoJS.enc.Utf8.parse(secretKey), {
-      mode: decryptionMode,
-      padding: CryptoJS.pad.Pkcs7,
-      iv: CryptoJS.enc.Utf8.parse(iv)
-    });
-    setDecryptedData(decrypted.toString(CryptoJS.enc.Utf8));
+    try {
+      const encryptedObject = outputMode === 'Base64' 
+        ? CryptoJS.lib.CipherParams.create({ ciphertext: CryptoJS.enc.Base64.parse(decryptInput) }) 
+        : CryptoJS.lib.CipherParams.create({ ciphertext: CryptoJS.enc.Hex.parse(decryptInput) });
+
+      const decrypted = CryptoJS.AES.decrypt(encryptedObject, CryptoJS.enc.Utf8.parse(secretKey), {
+        mode: decryptionMode,
+        padding: CryptoJS.pad.Pkcs7,
+        iv: CryptoJS.enc.Utf8.parse(iv)
+      });
+      setDecryptedData(decrypted.toString(CryptoJS.enc.Utf8));
+    } catch (error) {
+      setErrorMessage("The provided Secret Key is incorrect or the input is invalid.");
+      setDecryptedData(null);
+    }
   };
 
   const handleCopy = (data: string | null) => {
@@ -65,26 +76,35 @@ function App() {
 
   return (
     <div className="container mt-4">
-      <h2 className="text-center mb-4">Simple Encryption & Decryption using AES (mode ECB and CBC) with React.js Typescript</h2>
+      <h2 className="text-center mb-4">Simple Encryption & Decryption using AES (mode ECB and CBC) with Output Mode Base64 and Hex.</h2>
+      <hr />
+      <h4 className="text-center">Configuration</h4>
       <div className="row justify-content-center mb-4">
-        <div className="col-md-8 col-lg-6">
+        <div className="col-sm-4 col-md-3">
           <label className="form-label">Encryption Mode:</label>
           <select className="form-control mb-3" value={mode} onChange={(e) => setMode(e.target.value as 'ECB' | 'CBC')}>
             <option value="ECB">ECB</option>
             <option value="CBC">CBC</option>
           </select>
-          {mode === 'CBC' && (
-            <div className="mb-3">
-              <label className="form-label">Initialization Vector (16 characters):</label>
-              <input className="form-control" value={iv} onChange={(e) => setIV(e.target.value)} maxLength={16} />
-            </div>
-          )}
-          <div className="mb-3">
-            <label className="form-label">Secret Key (up to 32 characters):</label>
-            <input className="form-control" value={secretKey} onChange={(e) => setSecretKey(e.target.value)} maxLength={32} />
-            {errorMessage && <div className="text-danger mt-2">{errorMessage}</div>}
-          </div>
         </div>
+        <div className="col-sm-4 col-md-3">
+          <label className="form-label">Output Mode:</label>
+          <select className="form-control mb-3" value={outputMode} onChange={(e) => setOutputMode(e.target.value as 'Base64' | 'Hex')}>
+            <option value="Base64">Base64</option>
+            <option value="Hex">Hex</option>
+          </select>
+        </div>
+        {mode === 'CBC' && (
+          <div className="col-sm-4 col-md-3">
+            <label className="form-label">Initialization Vector (16 characters):</label>
+            <input className="form-control" value={iv} onChange={(e) => setIV(e.target.value)} maxLength={16} />
+          </div>
+        )}
+        <div className={`col-sm-4 col-md-${mode === 'ECB' ? '3' : '3'}`}>
+          <label className="form-label">Secret Key (32 characters):</label>
+          <input className="form-control" value={secretKey} onChange={(e) => setSecretKey(e.target.value)} maxLength={32} />
+        </div>
+        {errorMessage && <div className="text-danger mt-2 col-12">{errorMessage}</div>}
       </div>
       <div className="row">
         {/* Encryption Section (Left Side) */}
@@ -111,7 +131,7 @@ function App() {
         <div className="col-md-6">
           <h4 className="mb-3">Decryption</h4>
           <div className="mb-3">
-            <label className="form-label">Input for Decryption:</label>
+            <label className="form-label">Encrypted Data:</label>
             <input className="form-control" value={decryptInput} onChange={(e) => setDecryptInput(e.target.value)} />
           </div>
           <button className="btn btn-secondary" onClick={handleDecrypt}>Decrypt</button>
